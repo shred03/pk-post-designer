e74dcebced2c97de4919f51459387b2519abb9dd
shred03 <mithunyada12345@gmail.com>
2025-08-02 11:05:47 +0530

    added codec

--- main.py
+++ main.py
@@ -768,13 +768,13 @@ async def process_title_selection(callback_query: CallbackQuery, tmdb_id: str, m
                 'movie_p': title_data.get('name', 'N/A'),
                 'year_p': title_data.get('first_air_date', 'N/A')[:4] if title_data.get('first_air_date') else 'N/A',
             }
-            additional_message = f"""`[S{{season}}E{{episode}}] {series_data['movie_p']} {{quality}}`
+            additional_message = f"""`[S{{season}}E{{episode}}] {series_data['movie_p']} {{quality}} {{codec}}`
             
-`[S{{season}}E{{episode}}] {series_data['movie_p']} {{quality}}`
+`[S{{season}}E{{episode}}] {series_data['movie_p']} {{quality}} {{codec}}`
             
-`[S01E{{episode}}] {series_data['movie_p']} {{quality}}`
+`[S01E{{episode}}] {series_data['movie_p']} {{quality}} {{codec}}`
 
-` -n [S0][EP] {series_data['movie_p']} ({series_data['year_p']}).mkv`"""
+` -n [S01][EP] {series_data['movie_p']} ({series_data['year_p']}).mkv`"""
             
             caption = format_series_caption(
                 movie=title_data.get('name', 'N/A'),
